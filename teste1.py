0c6de7819f7cbc2ff3dfb3c775885e15ad930cb7
vitordev <106778777+vitornine@users.noreply.github.com>
2023-08-15 18:16:07 -0300

    Add files via upload

---
+++ teste1.py
@@ -0,0 +1,13 @@
+a = 33
+b = "33"
+if b > a:
+    print("b é maior que a")
+elif b == a:
+    print("a e b são iguais")
+
+    def my_function(fname):
+        print(fname = "Refsnes")
+
+        my_function("Emily")
+        my_function("Tobias")
+        my_function("Lucas")
